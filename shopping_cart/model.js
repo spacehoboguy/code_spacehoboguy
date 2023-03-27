063f35f6a483ab1922c0ee88364dd686f2c7aac3
Philip Olafsen <ph.olafsen@gmail.com>
2023-03-27 17:53:09 +0200

    updated view and model. Working on adding inputs to change the model and objects later.

--- shopping_cart/model.js
+++ shopping_cart/model.js
@@ -1,3 +1,11 @@
+let cartTotal = 0;
+let addedTotal;
+let cartTotalView = `<tr>
+                      <td><b>Total Price</b></td>
+                      <td></td>
+                      <td></td>
+                      <td><b>${cartTotal}kr</b></td>
+                    </tr>`;
 
 let shoppingCart = [
   {
@@ -9,5 +17,20 @@ let shoppingCart = [
     itemName: "Banan",
     itemPrice: 8,
     noOfItems: 4,
+  },
+  {
+    itemName: "Mango",
+    itemPrice: 12,
+    noOfItems: 3, 
+  },  
+  {
+    itemName: "Ananas",
+    itemPrice: 20,
+    noOfItems: 2, 
+  },  
+  {
+    itemName: "Mandarin",
+    itemPrice: 8,
+    noOfItems: 3, 
   }  
 ]
