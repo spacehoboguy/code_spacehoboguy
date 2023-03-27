ea1b62cda8484c5320eb64760ba37d7f4afacb04
Philip Olafsen <ph.olafsen@gmail.com>
2023-03-27 22:20:25 +0200

    ~change: changed the view structure to modular

--- shopping_cart/view.js
+++ shopping_cart/view.js
@@ -1,28 +1,18 @@
 updateView()
 function updateView() {
+
+    if (appState.currentView == "shopping_cart") {
+        
+        appState.currentView = "shopping_cart";
+        
+        
+        generateShoppingCartView()
+        calculateSumOfCart()
+        cartHtml += cartTotalView
     
-    let html =
-        `<h1>Fruit Basket</h1>
-        <table>
-        <tr>
-        <th>Product</th>
-        <th>Amount</th>
-        <th>Price</th>
-        <th>Sum</th>
-        </tr> `
-    for (let i = 0; i < shoppingCart.length; i++) {
-        html +=
-            `<tr>
-                <td>${shoppingCart[i].itemName}</td>
-                <td class ="numberOfItems">${shoppingCart[i].noOfItems}</td>
-                <td>${shoppingCart[i].itemPrice} kr</td>
-                <td><b>${shoppingCart[i].noOfItems * shoppingCart[i].itemPrice} kr</b></td>
-            </tr>`
+    } else {
+
     }
-    calculateSumOfCart()
-    html += cartTotalView
 
-
-
-    document.getElementById("app").innerHTML = html;
+    document.getElementById("app").innerHTML = userView;
 }
