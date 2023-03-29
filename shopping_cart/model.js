338e959a64a88dfa5c026549700004a67781607e
Philip Olafsen <ph.olafsen@gmail.com>
2023-03-29 09:39:29 +0200

    added imgs for fruit store and updated the model

--- shopping_cart/model.js
+++ shopping_cart/model.js
@@ -15,26 +15,31 @@ let storeContent = [
     itemName: "Apple",
     itemPrice: 10,
     noOfItems: 1,
+    itemSrc: "apple.jpg",
   },
   {
     itemName: "Banana",
     itemPrice: 8,
     noOfItems: 1,
+    itemSrc: "banana.jpg",
   },
   {
     itemName: "Mango",
     itemPrice: 12,
     noOfItems: 1,
+    itemSrc: "mango.jpg",
   },
   {
     itemName: "Pineapple",
     itemPrice: 20,
     noOfItems: 1,
+    itemSrc: "pineapple.jpg",
   },
   {
     itemName: "Tangerine",
     itemPrice: 8,
     noOfItems: 1,
+    itemSrc: "tangerine.jpg",
   }
 ]
 let shoppingCart = [
